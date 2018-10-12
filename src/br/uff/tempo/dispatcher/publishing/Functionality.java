6fe345e5af28654a4133f80977a0463b54c0562a
Heder Dorneles <hederdorneles@users.noreply.github.com>
2018-10-12 01:51:50 -0300

    Update and rename src/br/uff/tempo/dispatcher/Functionality.java to src/br/uff/tempo/dispatcher/publishing/Functionality.java

--- src/br/uff/tempo/dispatcher/publishing/Functionality.java
+++ src/br/uff/tempo/dispatcher/publishing/Functionality.java
@@ -1,33 +1,37 @@
-package br.uff.tempo.dispatcher;
+package br.ic.uff.tempo.dispatcher.publishing;
 
 public class Functionality {
-	
+
 	private double value = 0;
 	private boolean isBusy = false;
 	private String description = new String();
-	
+
+	public Functionality() {
+
+	}
+
 	public double getValue() {
 		return value;
 	}
-	
+
 	public void setValue(double value) {
 		this.value = value;
 	}
-	
+
 	public boolean isBusy() {
 		return isBusy;
 	}
-	
+
 	public void setBusy(boolean isBusy) {
 		this.isBusy = isBusy;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 }
