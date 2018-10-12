586bcccd6b0a58b57ac25ba4c60a564a1d1196a9
Heder Dorneles <hederdorneles@users.noreply.github.com>
2018-10-12 01:50:22 -0300

    Update and rename src/br/uff/tempo/dispatcher/Device.java to src/br/uff/tempo/dispatcher/publishing/Device.java

--- src/br/uff/tempo/dispatcher/publishing/Device.java
+++ src/br/uff/tempo/dispatcher/publishing/Device.java
@@ -1,4 +1,4 @@
-package br.uff.tempo.dispatcher;
+package br.ic.uff.tempo.dispatcher.publishing;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -6,14 +6,25 @@ import java.util.Iterator;
 public class Device {
 
 	private String id = new String();
+	private String description = new String();
 	private ArrayList<Functionality> functionalities = new ArrayList<Functionality>();
+	private Ambient ambient = new Ambient();
 
 	public String getId() {
 		return id;
 	}
+	
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public ArrayList<Functionality> getFunctionalities() {
@@ -24,6 +35,14 @@ public class Device {
 		this.functionalities = functionalities;
 	}
 
+	public Ambient getAmbient() {
+		return ambient;
+	}
+
+	public void setAmbient(Ambient ambient) {
+		this.ambient = ambient;
+	}
+	
 	public Functionality findFunctionality(String description) {
 		for (Iterator<Functionality> iterator = this.functionalities.iterator(); iterator.hasNext();) {
 			Functionality ts = iterator.next();
