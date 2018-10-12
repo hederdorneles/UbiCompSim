e354753ff006f2eb3326deba3e2c4e0766969a21
Heder Dorneles <hederdorneles@users.noreply.github.com>
2018-10-12 01:55:37 -0300

    Create SubscriberImpl.java

---
+++ src/br/uff/tempo/dispatcher/subscribingService/SubscriberImpl.java
@@ -0,0 +1,17 @@
+package br.ic.uff.tempo.dispatcher.subscribingService;
+
+public class SubscriberImpl extends Subscriber {
+
+	public void addSubscriber(String topic, Dispatcher pubSubService) {
+		pubSubService.addSubscriber(topic, this);
+	}
+
+	public void unSubscribe(String topic, Dispatcher pubSubService) {
+		pubSubService.removeSubscriber(topic, this);
+	}
+
+	public void getMessagesForSubscriberOfTopic(String topic, Dispatcher pubSubService) {
+		pubSubService.getMessagesForSubscriber(topic, this);
+
+	}
+}
